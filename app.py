2a463d5c3170b448f394cad45909b34e29f825a7
Ubeyt Demir <190541075@firat.edu.tr>
2021-11-10 13:33:45 +0300

    adds blog add & list

--- app.py
+++ app.py
@@ -1,17 +1,50 @@
 # import flask 
 
-from flask import Flask,render_template,request
+from dotenv import load_dotenv
+from os import environ
+from flask import Flask,render_template,request,redirect,url_for
 from flask_ckeditor import CKEditor
-
+from flask_sqlalchemy import SQLAlchemy
+from slugify import slugify
+import uuid
 # create the application object
 
+load_dotenv()
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db.sqlite"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"]=True
+db = SQLAlchemy(app)
 ckeditor = CKEditor()
 ckeditor.init_app(app)
 
+
+class Blogs(db.Model):
+    id = db.Column(db.String, primary_key=True, autoincrement=True)
+    title = db.Column(db.String, nullable=False)
+    slug = db.Column(db.String, unique=True, nullable=False)
+    content = db.Column(db.String,nullable=False)
+    category = db.Column(db.String,nullable=False)
+    cover_url = db.Column(db.String,nullable=False)
+    
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "title": self.title,
+            "slug": self.slug,
+            "content": self.content,
+            "category": self.category,
+            "cover_url": self.cover_url
+        }
+ 
+
+
+
+ 
+
 @app.route("/")
 def home():
-    return render_template("home.html")
+    blogs = [blog.to_dict() for blog in Blogs.query.all()]
+    return render_template("home.html",blogs=blogs)
 
 @app.route("/search")
 def search():
@@ -47,12 +80,16 @@ def add_new_blog():
         ## get article by sluf if its found render
         return render_template("new.html")
     else:
-        data = request.form.get("ckeditor")
+        content = request.form.get("ckeditor")
         title = request.form.get("title")
         category = request.form.get("category")
-        print(data)
+        cover_url = request.form.get("cover_url")
+        new_blog = Blogs(id=str(uuid.uuid4()),title=title,category=category,cover_url=cover_url,slug=slugify(title),content=content)
+        db.session.add(new_blog)
+        db.session.commit()
+        print(new_blog)
         ## get article by sluf if its found render
-        return render_template("new.html")
+        return  redirect_url(url_for("home"))
 
 
 if __name__ == "__main__":
